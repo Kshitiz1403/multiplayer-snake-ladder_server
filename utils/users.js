def1333c12f7013cf643ea91f89730625a18704c
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-04-21 02:14:14 +0530

    feat✨: add player number functionality

--- utils/users.js
+++ utils/users.js
@@ -1,7 +1,16 @@
 const users = []
 
 export const userJoin = (id, username, room) => {
-    const user = { id, username, room }
+    // TODO -> handler correct room number
+    // TODO -> accept max 2 connections
+
+    let playerNumber = 0;
+    users.map(user => {
+        if (user.room === room) {
+            playerNumber++;
+        }
+    })
+    const user = { id, username, room, playerNumber: playerNumber + 1 }
 
     users.push(user)
 
