ac0bd0d51948e367e735ddb0bc8f5143c3aa48f0
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-04-21 02:14:28 +0530

    feat✨: add whose turn functionality

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ io.on('connection', (socket) => {
 
     socket.on('join_room', ({ userName, room }) => {
         const user = userJoin(socket.id, userName, room)
+        console.log(user)
         socket.join(user.room)
 
         // Send users and room info
@@ -29,10 +30,10 @@ io.on('connection', (socket) => {
 
 
     // Listen for updates in location
-    socket.on('send-update', (location, dice) =>{
+    socket.on('send-update', (location, dice, this_turn) =>{
         console.log(location, dice)
         const user = getCurrentUser(socket.id)
         console.log(user)
-        socket.broadcast.to(user.room).emit('receive-update', {location, dice})
+        socket.broadcast.to(user.room).emit('receive-update', {location, dice, this_turn})
     })
 })
