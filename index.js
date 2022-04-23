79b502ff45c0c93b141aba8ede5fa5098792d3dc
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-04-24 00:51:43 +0530

    fix🐛: rollback - server wont listen to 6000 (still doesn't)

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const PORT = process.env.PORT || 6000
+const PORT = process.env.PORT || 5000
 
-app.use(express.static(__dirname +  '/../Client/build'))
+app.use(express.static(__dirname +  '\\..\\Client\\build'))
+
 const server = app.listen(PORT, () => console.log('listening at',PORT))
 
 const io = socketServer(server)
